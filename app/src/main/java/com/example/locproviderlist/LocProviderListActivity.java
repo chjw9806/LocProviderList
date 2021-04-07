6eae1d749c64b22c14d2e8dd8ec3424262a6520b
82103 <chjw9806@naver.com>
2021-04-07 16:19:48 +0900

    enhanced for loop

--- app/src/main/java/com/example/locproviderlist/LocProviderListActivity.java
+++ app/src/main/java/com/example/locproviderlist/LocProviderListActivity.java
@@ -2,6 +2,7 @@ package com.example.locproviderlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
@@ -23,20 +24,26 @@ public class LocProviderListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mtextView = findViewById(R.id.txtOutput);
-
         mbutton = findViewById(R.id.button);
+
         mbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 locMgr = (LocationManager) getSystemService(LOCATION_SERVICE); // new LocationManager() 만드는거
                 locProviders = locMgr.getAllProviders();
-                String s = "";
+                //String s = "";
+
+                for (String location: locProviders){
+                    mtextView.append("Loc. Provider: " + locProviders.size() + "\n");
+                    mtextView.append("Status: " + locMgr.isProviderEnabled(String.valueOf(location)) + "\n\n");
+                }
+/*
                 for (int i = 0; i < locProviders.size(); i++) {
                     s += "Loc. Provider: " + locProviders.get(i) + "\n"
                             + "Status: " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
 
                     mtextView.setText(s);
-                }
+                }*/
             }
         });
 }
